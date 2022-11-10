123784fe79af8bdc460da7193fb293fc632d6a74
Chris Ochieng <jangita.nyagudi@gmail.com>
2022-11-10 16:40:01 +0000

    Second commit

--- run.py
+++ run.py
@@ -3,8 +3,8 @@ import ccxt
 import config
 
 xchange = ccxt.ftx({
-        'apiKey': '',
-        'secret': '',
+        'apiKey': '-P',
+        'secret': 'sL-',
     })
 
 def getPrice():
@@ -19,5 +19,63 @@ def sell(price,amount):
 
 # Create starting trade and wait for execution
 price = getPrice()
-print(price)
+if config.DIRECTION == config.dLong:
+    start = buy(price * (1 - config.BASE_DISTANCE), config.BASE)
 
+if config.DIRECTION == config.dShort:
+    start = sell(price * (1 + config.BASE_DISTANCE), config.BASE)
+
+basePrice = price
+
+while xchange.fetch_order(start['id'])['status'] != 'closed':
+    pass
+
+takeProfitOrder = None
+takeProfitPrice = None
+
+safetyOrder = None
+safetyOrderPrice = None
+
+safetyDone = 0
+safetySize = None
+
+positionSize = config.BASE
+
+# Create first safety order and take profit order
+price = getPrice()
+safetySize = positionSize * config.SAFETY_PRICE_PERCENT
+
+if config.DIRECTION == config.dLong:
+    takeProfitPrice = basePrice *  (1 + config.PROFIT_PERCENT)
+    takeProfitOrder = sell(takeProfitPrice , positionSize)
+
+    safetyOrderPrice = basePrice * (1 - config.SAFETY_PRICE_DISTANCE)
+    safetyOrder = buy(safetyOrderPrice, safetySize)
+
+if config.DIRECTION == config.dShort:
+    takeProfitPrice = basePrice *  (1 - config.PROFIT_PERCENT)
+    takeProfitOrder = buy(takeProfitPrice , positionSize)
+
+    safetyOrderPrice = basePrice * (1 + config.SAFETY_PRICE_DISTANCE)
+    safetyOrder = sell(safetyOrderPrice, safetySize)
+
+
+
+# while True:
+#     price =getPrice()
+#     orders = xchange.fetch_orders()
+    
+#     # Check and update takeProfit order
+#     if takeProfitOrder is None:
+#         if config.DIRECTION == config.dLong:
+#             takeProfitPrice = basePrice *  (1 + config.PROFIT_PERCENT)
+#             takeProfitOrder = sell(takeProfitPrice , positionSize)
+
+#         if config.DIRECTION == config.dShort:
+#             takeProfitPrice = basePrice *  (1 - config.PROFIT_PERCENT)
+#             takeProfitOrder = buy(takeProfitPrice , positionSize)
+
+#     # Check and update Safety Order
+#     if safetyOrder is None:
+#         if config.DIRECTION == config.dLong:
+#             safetyOrderPrice = basePrice * (1 - config.SAFETY_PRICE_PERCENT)
